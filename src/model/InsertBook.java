76583a3b8df3d862f732c98d58df725690a0b42f
Justus Brown <jbrow21@brockport.edu>
2019-03-05 20:22:42 -0500

    Db inserts and state checks/inserts

--- src/model/InsertBook.java
+++ src/model/InsertBook.java
@@ -20,7 +20,6 @@ public class InsertBook implements IView, IModel {
 	// For Impresario
 	private Properties dependencies;
 	private ModelRegistry myRegistry;
-
 	private AccountHolder myAccountHolder;
 
 	// GUI Components
@@ -30,6 +29,10 @@ public class InsertBook implements IView, IModel {
 	private String loginErrorMessage = "";
 	private String transactionErrorMessage = "";
 		
+	public static final String INSERT_BOOK_BACK = "InsertBookBack";
+	public static final String INSERT_BOOK_INSERT = "InsertBookInsert";
+	public static final String INSERT_BOOK_VIEW = "InsertBookView";
+	
 	public InsertBook() {
 		myStage = MainStageContainer.getInstance();
 		myViews = new Hashtable<String, Scene>();
@@ -50,10 +53,8 @@ public class InsertBook implements IView, IModel {
 		myRegistry.setDependencies(dependencies);
 	}
 	
-	public void updateState(String key, String val) {
-		
-	}
 	public void showView() { createAndShowInsertBookView(); }
+	public void closeView() { createAndShowTellerView(); }
 	
 	/**
 	 * Method called from client to get the value of a particular field
@@ -71,6 +72,10 @@ public class InsertBook implements IView, IModel {
 		return null;
 	}
 	
+	/**
+	 * Creates the InsertBookView if not already created. Then shows 
+	 * the book view.
+	 */
 	private void createAndShowInsertBookView() {
 		Scene currentScene = (Scene)myViews.get("InsertBookView");
 		if (currentScene == null) {
@@ -82,10 +87,28 @@ public class InsertBook implements IView, IModel {
 		swapToView(currentScene);
 	}
 	
+	private void createAndShowTellerView() {
+		Scene currentScene = (Scene)myViews.get("TellerView");
+		if (currentScene == null) {
+			// Create our initial view
+			View newView = ViewFactory.createView("TellerView", this);
+			currentScene = new Scene(newView);
+			myViews.put("TellerView", currentScene);
+		}
+		swapToView(currentScene);
+	}
+	
+	
 	@Override
+	/**
+	 * Sets the key value pairs within the models, respective to the field
+	 */
 	public void updateState(String key, Object value) {
-		// TODO Auto-generated method stub
-
+		if (dependencies.containsKey(key)) {
+			String str = (String) value;
+			dependencies.setProperty(key, str);
+			myRegistry.setDependencies(dependencies);
+		}
 	}
 	
 	/** Register objects to receive state updates. */
@@ -105,23 +128,43 @@ public class InsertBook implements IView, IModel {
 		// forward to our registry
 		myRegistry.unSubscribe(key, subscriber);
 	}
+	
+	/**
+	 * Depending on the key, handles various actions
+	 */
 	@Override
 	public void stateChangeRequest(String key, Object value) {
-		// TODO Auto-generated method stub
 		// get the values and update the model
-		if (key == null || value == null) { return; }
-		if (dependencies.containsKey("key")) {
-			String str = (String)value;
-			dependencies.setProperty(key, str);
-			myRegistry.setDependencies(dependencies);
+		if (key.equals(INSERT_BOOK_BACK)) {
+			createAndShowTellerView();
+		} else if (key.equals(INSERT_BOOK_INSERT)) {
+			insertBook();
+			System.out.println("Book's values: " + toString());
+		} else if (dependencies.containsKey(key)) {
+			updateState(key, value);
 		}
-		
 	}
-	public void swapToView(Scene newScene)
-	{
-		if (newScene == null)
-		{
-			System.out.println("Teller.swapToView(): Missing view for display");
+	private void insertBook() {
+		Properties p = new Properties();
+		
+		// For some reasons, this will prevent the book from beings added, removed temporarily
+		//p.setProperty("bookId", dependencies.getProperty("BookId"));
+		
+		p.setProperty("author",  dependencies.getProperty("Author"));
+		p.setProperty("title", dependencies.getProperty("Title"));
+		p.setProperty("pubYear", dependencies.getProperty("PubYear"));
+		p.setProperty("status", dependencies.getProperty("Status"));
+		Book b = new Book(p);
+		b.update();
+	}
+	
+	/**
+	 * Sets the given scene to the stage
+	 * @param newScene
+	 */
+	public void swapToView(Scene newScene) {
+		if (newScene == null) {
+			System.out.println("Insert.swapToView(): Missing view for display");
 			new Event(Event.getLeafLevelClassName(this), "swapToView",
 				"Missing view for display ", Event.ERROR);
 			return;
@@ -132,5 +175,8 @@ public class InsertBook implements IView, IModel {
 		
 		//Place in center
 		WindowPosition.placeCenter(myStage);
+	}
+	public String toString() {
+		return dependencies.toString();
 	}
 }
