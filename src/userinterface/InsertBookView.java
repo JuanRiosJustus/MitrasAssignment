41be06e0223af048af57f6b456b43762d8b78e69
Justus Brown <jbrow21@brockport.edu>
2019-03-14 13:41:21 -0400

    Updates to GUI components to match restrictions

--- src/userinterface/InsertBookView.java
+++ src/userinterface/InsertBookView.java
@@ -1,21 +1,29 @@
 package userinterface;
 
 import impresario.IModel;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import model.InsertBook;
 
 public class InsertBookView extends View
@@ -27,6 +35,8 @@ public class InsertBookView extends View
 	protected TextField titleField;
 	protected TextField pubYearField;
 	protected TextField statusField;
+	
+	protected ComboBox<String> statusComboBox;
 
 	protected Button backButton;
 	protected Button insertButton;
@@ -98,13 +108,13 @@ public class InsertBookView extends View
 
 		Text bookIdLabel = new Text("Id : ");
 		Font myFont = Font.font("Helvetica", FontWeight.BOLD, 12);
-		bookIdLabel.setFont(myFont);
+		/*bookIdLabel.setFont(myFont);
 		bookIdLabel.setWrappingWidth(150);
 		bookIdLabel.setTextAlignment(TextAlignment.RIGHT);
 		grid.add(bookIdLabel, 0, 1);
 
 		bookIdField = new TextField();
-		grid.add(bookIdField, 1, 1);
+		grid.add(bookIdField, 1, 1);*/
 
 		Text bookAuthorLabel = new Text("Author: ");
 		bookAuthorLabel.setFont(myFont);
@@ -139,14 +149,17 @@ public class InsertBookView extends View
 		statusLabel.setTextAlignment(TextAlignment.RIGHT);
 		grid.add(statusLabel, 0, 5);
 		
-		statusField = new TextField();
-		grid.add(statusField, 1, 5);
+		statusComboBox = new ComboBox<String>();
+		statusComboBox.setItems(FXCollections.observableArrayList("Active", "Inactive"));
+		statusComboBox.setValue("Active");
+		//statusField = new TextField();
+		grid.add(statusComboBox, 1, 5);
 		
 		
 
 		HBox doneCont = new HBox(10);
 		doneCont.setAlignment(Pos.CENTER);
-		backButton = new Button("Back");
+		backButton = new Button("Done");
 		backButton.setFont(Font.font("Arial", FontWeight.BOLD, 14));
 		backButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -157,19 +170,33 @@ public class InsertBookView extends View
 		});
 		doneCont.getChildren().add(backButton);
 		
-		insertButton = new Button("Insert");
+		insertButton = new Button("Submit");
 		insertButton.setFont(Font.font("Arial", FontWeight.BOLD, 14));
 		insertButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
 				clearErrorMessage();
+				if (authorField.getText().length() == 0 || titleField.getText().length() == 0) {
+					Alert a = new Alert(AlertType.ERROR);
+					a.setContentText("Please enter valid Author and/or Title");
+					a.show();
+					return; 
+				} else if (ensureIntegerValue(pubYearField.getText()) == false) {
+					Alert a = new Alert(AlertType.ERROR);
+					a.setContentText("Please enter a year between 1800 and 2019");
+					a.show();
+					return;
+				}
 				InsertBook ib = (InsertBook)myModel;
-				ib.updateState("BookId", bookIdField.getText());
+				//ib.updateState("BookId", bookIdField.getText());
 				ib.updateState("Title", titleField.getText());
 				ib.updateState("Author", authorField.getText());
 				ib.updateState("PubYear", pubYearField.getText());
-				ib.updateState("Status", statusField.getText());
-				myModel.stateChangeRequest(InsertBook.INSERT_BOOK_INSERT, null);   	
+				ib.updateState("Status", statusComboBox.getValue());
+				myModel.stateChangeRequest(InsertBook.INSERT_BOOK_INSERT, null); 
+				Alert a = new Alert(AlertType.CONFIRMATION);
+				a.setContentText("Book has been inserted.");
+				a.show();
 			}	
 		});
 		doneCont.getChildren().add(insertButton);
@@ -178,8 +205,16 @@ public class InsertBookView extends View
 		vbox.getChildren().add(doneCont);
 		return vbox;
 	}
-
-
+	
+	private boolean ensureIntegerValue(String pubYear) {
+		try {
+			int val = Integer.parseInt(pubYear);
+			return val >= 1800 && val <= 2019;
+		} catch (Exception ex) {
+			return false;
+		}
+	}
+	
 	// Create the status log field
 	//-------------------------------------------------------------
 	protected MessageView createStatusLog(String initialMessage)
